4cd4761436f130c3e75d79297cbb21c0bb10630e
Saad Boulaguiem <saad.boulaguiem@amadeus.com>
2022-06-12 14:13:11 +0200

    [top_k_frequent_elements] - optimal: counter list count[i] = [n,...] where n occurs i times

--- neetcode/top_k_frequent_elements.py
+++ neetcode/top_k_frequent_elements.py
@@ -1,10 +1,29 @@
 class Solution:
 
+    # author: Neetcode
+    # time: O(n)
+    # space: O(n)
+    def top_k_frequent_optimal(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
+        
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+    
     # author: Saad
     # time: O(nlog(n))
     # space: O(n)
     def top_k_frequent_sol1(self, nums: List[int], k: int) -> List[int]:
-        hash = defaultdict(lambda: 0)
+        count = defaultdict(lambda: 0)
         for n in nums:
-            hash[n] += 1
-        return [k for k,v in sorted(hash.items(), key=lambda pair: pair[1], reverse=True)][:k]
+            count[n] += 1
+        return [k for k,v in sorted(count.items(), key=lambda pair: pair[1], reverse=True)][:k]
